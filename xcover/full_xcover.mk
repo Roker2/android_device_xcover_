09352ae485952292f94c6a3927bbf57244625786
Roker2 (Dmitry Minko) <dimaminko2000@gmail.com>
2017-08-25 10:37:05 +0300

    Update full_xcover.mk

--- xcover/full_xcover.mk
+++ xcover/full_xcover.mk
@@ -3,13 +3,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/samsung/xcover/xcover.mk)
 
-# Screen density is actually considered a locale (since it is taken into account
-# the the build-time selection of resources). The product definitions including
-# this file must pay attention to the fact that the first entry in the final
-# PRODUCT_LOCALES expansion must not be a density.
 PRODUCT_LOCALES += mdpi
 
-# Discard inherited values and use our own instead.
 PRODUCT_NAME := full_xcover
 PRODUCT_DEVICE := xcover
 PRODUCT_BRAND := samsung
